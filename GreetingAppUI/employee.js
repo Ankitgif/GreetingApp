21c8da4766cd3ee11270b512e3510e8eb9392044
Ankitgif <ankitsinha2303@gmail.com>
2020-05-29 18:27:10 +0530

    [Ankit] Refactored code, and added button and image

--- GreetingAppUI/employee.js
+++ GreetingAppUI/employee.js
@@ -6,30 +6,14 @@ $.ajax({
         for (let i = 0; i < data.length; i++) {
             let row = $('<tr><td>' + data[i].firstname + '</td><td>'
                 + data[i].lastname + '</td><td>'
-                + data[i].email + '</td><td>'
-                + data[i].edit + '</td><td>'
-                + data[i].delete + '</td></tr>');
+                + data[i].email + '</td><td>' + "<button class=" + "addbtn" + ">" + '<img src = ' + data[i].edit + '>'
+                + "</button>" + '</td><td>' + "<button class=" + "addbtn" + ">" + '<img src = ' + data[i].delete + '>'
+                + "</button>" + '</td></tr>');
             $('.table').append(row);
         }
     },
     error: function (jqXHR, textStatus, errorThrown) { //the error parameter contains the function to be executed when the server request fails
         console.log('Error: ' + textStatus + ' - ' + errorThrown);          //textStatus - contains the status of the call
-                                                                            //xhr - contains the XMLHttpRequest object
+        //xhr - contains the XMLHttpRequest object
     }
 });
-
-// $(document).ready(function () {
-//     $.getJSON("data.json", function (data) {
-//         let employee_data = '';
-//         $.each(data, function (key, value) {
-//             employee_data += '<tr>';
-//             employee_data += '<td>' + value.firstname + '</td>';
-//             employee_data += '<td>' + value.lastname + '</td>';
-//             employee_data += '<td>' + value.email + '</td>';
-//             employee_data += '<td>' + value.edit + '</td>';
-//             employee_data += '<td>' + value.delete + '</td>';
-//             employee_data += '</tr>';
-//         });
-//         $('#employee_table').append(employee_data);
-//     });
-// });
